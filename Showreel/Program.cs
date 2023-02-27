870c093c09eebf6171ff8f970dd83eabf2c6a7d2
Anders Hesselbom <anders@winsoft.se>
2023-02-27 18:43:48 +0100

    Animations added.

---
+++ Showreel/Program.cs
@@ -0,0 +1,8 @@
+﻿using MutableStringLibrary;
+
+var helloWorld = new MutableString("Hello world!");
+var has = helloWorld.Has("Ello", out var start, out var length);
+
+Console.WriteLine(has);
+Console.WriteLine(start);
+Console.WriteLine(length);
